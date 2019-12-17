1407d03e16914644f3263acafd14e009e79a9392
Dan McAdams <danmcadams@gmail.com>
2019-12-16 22:48:28 -0600

    change method name from extractExpectedPartialsFrom to extractExpectedPartialsFromTemplate

--- src/Certificate/RenderHelper.php
+++ src/Certificate/RenderHelper.php
@@ -6,7 +6,7 @@ namespace HomeCEU\Certificate;
 
 class RenderHelper
 {
-    public static function extractExpectedPartialsFrom(string $template): array
+    public static function extractExpectedPartialsFromTemplate(string $template): array
     {
         preg_match_all('/{{>([^\}}]+)}}/', $template, $matches);
         return !empty($matches[1]) ? array_map('trim', $matches[1]) : [];
