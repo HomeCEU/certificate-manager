7ef7ce14ef2c387f04ab56a062bf7fa60838ec00
Dan McAdams <danmcadams@gmail.com>
2019-12-19 10:47:37 -0600

    add NoTemplateProvided and NonPartialException

---
+++ src/Certificate/Exception/NoTemplateProvidedException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+
+namespace HomeCEU\Certificate\Exception;
+
+
+class NoTemplateProvidedException extends \Exception
+{
+}
---
+++ src/Certificate/Exception/NonPartialException.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+
+namespace HomeCEU\Certificate\Exception;
+
+
+class NonPartialException extends \Exception
+{
+}
