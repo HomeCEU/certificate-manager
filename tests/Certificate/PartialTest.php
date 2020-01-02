b03873463f7157398aba365fdb8a6dc3494e6363
Dan McAdams <danmcadams@gmail.com>
2020-01-02 13:34:06 -0600

    update PSR-4 namespaces

--- tests/Certificate/PartialTest.php
+++ tests/Certificate/PartialTest.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
 
-namespace HomeCEU\Certificate\Tests;
+namespace HomeCEU\Tests;
 
 
 use HomeCEU\Certificate\Partial;
---
+++ tests/Certificate/TemplateStorageTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+
+
+namespace HomeCEU\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class TemplateStorageTest extends TestCase
+{
+    public function testRuns(): void
+    {
+        $this->assertTrue(true);
+    }
+}
