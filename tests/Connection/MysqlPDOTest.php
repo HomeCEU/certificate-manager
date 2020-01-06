f686828093f9fa6cd2ebf653d89a93f92c38323a
Dan McAdams <danmcadams@gmail.com>
2020-01-06 15:16:40 -0600

    remove dump

--- tests/Connection/MysqlPDOTest.php
+++ tests/Connection/MysqlPDOTest.php
@@ -12,7 +12,6 @@ class MysqlPDOTest extends TestCase
     public function testCreateFromConfiguration(): void
     {
         $config = include(__DIR__ . '/../../config/local/db_config.php');
-        dump($config['mysql']);
 
         $pdo = MysqlPDOConnection::createFromConfig($config['mysql']);
         $this->assertInstanceOf(\PDO::class, $pdo);
