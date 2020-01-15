9440954a53621c36cc5e6695965fcad75c34b1a0
Dan McAdams <danmcadams@gmail.com>
2020-01-15 10:22:39 -0600

    change MYSQL_PASS to MYSQL_PASSWORD

--- config/sample/db_config.sample.php
+++ config/sample/db_config.sample.php
@@ -4,7 +4,7 @@ return [
     'mysql' => [
         'host' => getenv('MYSQL_HOST'),
         'user' => getenv('MYSQL_USER'),
-        'password' => getenv('MYSQL_PASS') ?: '',
+        'password' => getenv('MYSQL_PASSWORD') ?: '',
         'port' => getenv('MYSQL_PORT') ?: 3306,
         'database' => getenv('MYSQL_DATABASE'),
     ]
