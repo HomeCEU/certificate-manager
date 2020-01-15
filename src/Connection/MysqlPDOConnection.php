8dc5e8b2376a1cf5c70a28b66c7b2f12c2ee4a76
Dan McAdams <danmcadams@gmail.com>
2020-01-15 13:38:52 -0600

    add port to PDO dsn string

--- src/Connection/MysqlPDOConnection.php
+++ src/Connection/MysqlPDOConnection.php
@@ -9,7 +9,7 @@ class MysqlPDOConnection
     public static function createFromConfig(array $config): \PDO
     {
         return new \PDO(
-            "mysql:host={$config['host']};dbname={$config['database']}",
+            "mysql:host={$config['host']};port={$config['port']};dbname={$config['database']}",
             $config['user'],
             $config['password']
         );
