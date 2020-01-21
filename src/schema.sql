df7bc29fb25b79bef2aff3c3a512d08342254024
dan <danmcadams@gmail.com>
2020-01-21 14:44:46 -0600

    change body from text to mediumtext

--- src/schema.sql
+++ src/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS template
     template_id VARCHAR(255) NOT NULL,
     constant    VARCHAR(255) NOT NULL,
     name        VARCHAR(255) NOT NULL,
-    body        TEXT         NOT NULL,
+    body        MEDIUMTEXT   NOT NULL,
     created_on  DATETIME DEFAULT NOW(),
     updated_on  DATETIME,
     created_by  INT,
