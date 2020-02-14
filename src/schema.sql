b3363071af3617e270b8da48c259357d26f440be
dan <danmcadams@gmail.com>
2020-02-14 13:43:36 -0600

    change dock_type column name to document_type

--- src/schema.sql
+++ src/schema.sql
@@ -13,25 +13,25 @@ CREATE TABLE IF NOT EXISTS document_type
 
 CREATE TABLE IF NOT EXISTS template
 (
-    template_id VARCHAR(255) NOT NULL,
-    constant    VARCHAR(255) NOT NULL,
-    name        VARCHAR(255) NOT NULL,
-    body        MEDIUMTEXT   NOT NULL,
-    created_on  DATETIME DEFAULT NOW(),
-    updated_on  DATETIME,
-    created_by  INT,
-    type        VARCHAR(255),
+    template_id   VARCHAR(255) NOT NULL,
+    constant      VARCHAR(255) NOT NULL,
+    name          VARCHAR(255) NOT NULL,
+    body          MEDIUMTEXT   NOT NULL,
+    created_on    DATETIME DEFAULT NOW(),
+    updated_on    DATETIME,
+    created_by    INT,
+    document_type VARCHAR(255),
 
     PRIMARY KEY (template_id),
-    FOREIGN KEY (type) REFERENCES document_type (constant)
+    FOREIGN KEY (document_type) REFERENCES document_type (constant)
 );
 
 CREATE TABLE IF NOT EXISTS document_data
 (
-    data_key VARCHAR(255) NOT NULL,
-    dock_type VARCHAR(255) NOT NULL,
-    data TEXT,
+    data_key      VARCHAR(255) NOT NULL,
+    document_type VARCHAR(255) NOT NULL,
+    data          TEXT,
 
-    PRIMARY KEY (dock_type, data_key),
-    FOREIGN KEY (dock_type) REFERENCES document_type (constant)
+    PRIMARY KEY (document_type, data_key),
+    FOREIGN KEY (document_type) REFERENCES document_type (constant)
 );
